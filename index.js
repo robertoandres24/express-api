36deedf5a951034494bf9b3dd2e830ccccbb0359
robertoandres24 <robertoandres24@gmail.com>
2020-10-25 17:29:41 -0300

    add api crud and saving in mongo

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('yayys');
+
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const app = express();
+
+// Db connection
+const { mongoose } = require('./database');
+
+// Settings 
+app.set('port', process.env.PORT || 3000);
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+// Routes
+app.use('/api/tasks', require('./routes/task.routes'));
+
+// Static Files
+app.use(express.static(path.join(__dirname, 'public')));;
+
+// Starting the server
+app.listen(app.get('port'), () => {
+  console.log(`Example app listening at http://localhost:${app.get('port')}`)
+});
